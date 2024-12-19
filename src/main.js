93f7ee322b99c41672f9b60adb2f4042504f0928
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2024-12-19 20:31:09 +0100

    update project

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header.js';
+import './js/about-me.js';
+import './js/projects.js';
+import './js/faq.js';
+import './js/covers.js';
+import './js/reviews.js';
+import './js/work-together.js';
